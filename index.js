36b161be077c93332f0ae38d8dba2efe9b633e58
Sahana-Lawrence <sahana.lawrence@mths.ca>
2020-05-29 00:54:30 +0000

    updated files

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 // JavaScript File
 
-// variables assigned to calculate the area and circumference of a circle
-function Area(){
-var diameter = document.getElementById("diameter");
-var radius = document.getElementById("radius");
-var diametervalue = +diameter.value;
-var radiusvalue = +(diameter.value/2);
-var result1 = ((diameter.value/2)*(diameter.value/2)*Math.PI);
-var result2  = (diameter.value*Math.PI);
-var divresult1 = document.getElementById("divresult1");
-var divresult2 = document.getElementById("divresult2");
-divresult1.innerHTML = result1;
-divresult2.innerHTML = result2;
+document.getElementById("submitguess").onclick = function() {
+var age = document.getElementById("age");
+var agevalue = age.value;
+
+ if (age.value >= 17) {
+   alert("You can watch R-rated movies!");
 }
+ else if (age.value >= 13) {
+   alert("You can watch PG-13 movies alone!");
+}
+ else {
+  alert("You can watch G-rated movies alone!");
+}
+};
